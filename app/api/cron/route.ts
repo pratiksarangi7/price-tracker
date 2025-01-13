d2f723e800ba0aa13e5dcc35af5fc1d46d45c501
Pratik <sarangipratikcode@gmail.com>
2025-01-13 23:01:22 +0530

    deployment fix 3

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,14 +11,13 @@ import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 60; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export async function GET(request: Request) {
   try {
     connectToDB();
-
     const products = await Product.find({});
 
     if (!products) throw new Error("No product fetched");
